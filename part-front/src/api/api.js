f464cd41a890286b9ba8cd761395c151bd310242
m-benrhabra <meriem.benrhabra1@gmail.com>
2021-05-24 12:33:10 +0100

    detete folder

--- part-front/src/api/api.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-const url = 'http://localhost:8080/admins/dashboard';
-
-export const fetchUsers = () => axios.get(url);
--- part-front/src/redux/constante/actionType.js
+++
@@ -1,3 +0,0 @@
-export const UPDATE = 'UPDATE';
-export const DELETE = 'DELETE';
-export const FETCH_ALL = 'FETCH_ALL';
